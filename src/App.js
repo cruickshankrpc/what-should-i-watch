ec8b576727dccb3d427603c97ccefb491e531923
Raquel <cruickshankrpc@gmail.com>
2021-02-01 18:36:24 +0000

    footer (to edit)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import cursor from './cursor.png'
+import cursor from './cursor.png';
+import Footer from './Footer';
+
 function App() {
   return (
     <div className="App">
+
       <div className="home_box">
         <p className="home_text">What the f*** should I watch ?</p>
       </div>
       <div ></div>
       <img className="cursor_img" src={cursor} alt="cursor" />
+      {/* <div component={Footer}></div> */}
+      <Footer></Footer>
     </div>
   );
 }
