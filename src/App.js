5c078d933dcefbc82929359a3d4f4969fe2834d7
Raquel <cruickshankrpc@gmail.com>
2021-02-01 17:47:43 +0000

    homepage complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import cursor from './cursor.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="home_box">
+        <p className="home_text">What the f*** should I watch ?</p>
+      </div>
+      <div ></div>
+      <img className="cursor_img" src={cursor} alt="cursor" />
     </div>
   );
 }
